54641c97a98398a9c2e25c6a484e2e22509ffae4
Angel Tapia Ramírez <angel.t.r@hotmail.com>
2025-10-30 17:16:04 -0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,64 @@
+import { House, User, Folder, Mail, Github, Linkedin, Email } from "@deemlol/next-icons";
 import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+
+    // HEADER
+    <div>
+      <div className="flex items-center justify-between p-2 font-sans border-b border-zinc-200 dark:border-zinc-800 absolute w-full bg-zinc-50 dark:bg-black text-black dark:text-white">
+        <div className="flex flex-row gap-1 w-full items-center justify-center uppercase tracking-widest">
+          <a href="" className="text-red-800">{"<Portfolio>"}</a>
+        </div>
+      </div>
+
+      {/* CUERPO PRINCIPAL */}
+      <div className="flex flex-row h-dvh">
+        <div className="w-8 flex flex-col gap-5 justify-end lg:justify-center items-center border-r py-20 lg:py-0 border-zinc-200 dark:border-zinc-800">
+          <a href="">
+            <House size={18} />
+          </a>
+          <a href="">
+            <User size={18} />
+          </a>
+          <a href="">
+            <Folder size={18} />
+          </a>
+          <a href="">
+            <Mail size={18} />
+          </a>
+        </div>
+
+        <div className="flex items-center justify-center bg-zinc-50 font-sans dark:bg-black w-svw">
+          <div className="text-4xl tracking-widest p-10">
+            <p>Hi, I,m Angel,</p>
+            <p>the solution to your problems.</p>
+          </div>
+        </div>
+      </div>
+
+      {/* FOOTER */}
+      <div>
+        <div className="flex items-center justify-between px-10 py-2 font-sans border-t border-zinc-200 dark:border-zinc-800  w-full bg-zinc-50 dark:bg-black text-gray-300 dark:text-gray-500 absolute bottom-0 ">
+          <p className="tracking-widest capitalize">
+            Angel T. Ramírez
           </p>
+          <div className="flex flex-row gap-5 text-sm tracking-widest">
+            <a href="" className="flex gap-2">
+              <p className="hidden md:block">Github</p>
+              <Github size={18} />
+            </a>
+            <a href="" className="flex gap-2">
+              <p className="hidden md:block">LinkedIn</p>
+              <Linkedin size={18} />
+            </a>
+            <a href="" className="flex gap-2">
+              <p className="hidden md:block">Email</p>
+              <Email size={18} />
+            </a>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+
+      </div>
     </div>
   );
 }
